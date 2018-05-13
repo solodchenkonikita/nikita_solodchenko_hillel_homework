1c1912839bb27474c69a0e1dcd3d189224315403
solodchenkonikita <solodchenkonikita@gmail.com>
2018-05-13 22:54:05 +0300

    done HW1

---
+++ homework1/src/task2.java
@@ -0,0 +1,6 @@
+public class task2 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 0; i--)
+            System.out.println(args[i]);
+    }
+}
---
+++ homework1/src/task3.java
@@ -0,0 +1,11 @@
+public class task3 {
+    public static void main(String[] args) {
+        int [] numbers =  { 1, 2, 3, 4, 5, 6 };
+        for ( int i = 0; i < numbers.length; i++ ){
+            System.out.println(numbers[ i ]);
+        }
+        for ( int i = 0; i < numbers.length; i++ ){
+            System.out.print(numbers[ i ] + " ");
+        }
+    }
+}
